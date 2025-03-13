15bc1e7d2676b910b1bb1d3bb3d69768b46e5a3f
PARZIV4L <66357974+Kahariel@users.noreply.github.com>
2025-03-14 02:37:33 +0800

    initial

---
+++ js/login.js
@@ -0,0 +1,8 @@
+var form = document.getElementById("login");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+
+function login() {
+  alert()
+}
